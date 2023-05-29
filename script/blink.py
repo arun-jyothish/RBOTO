d58fd59977f1c93758ef76751e11f3f57ec32003
arunjyothishk <arunjyothish.mec@gmail.com>
2023-05-30 01:59:52 +0530

    servo_pin change

--- script/blink.py
+++ script/blink.py
@@ -2,11 +2,11 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from time import sleep # Import the sleep function from the time module
 
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setmode(GPIO.BCM) # Use Broadcom pin numbering
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
-
+servo_pin = 23
 while True: # Run forever
-    GPIO.output(8, GPIO.HIGH) # Turn on
+    GPIO.output(servo_pin, GPIO.HIGH) # Turn on
     sleep(1) # Sleep for 1 second
-    GPIO.output(8, GPIO.LOW) # Turn off
+    GPIO.output(servo_pin, GPIO.LOW) # Turn off
     sleep(1) # Sleep for 1 second
